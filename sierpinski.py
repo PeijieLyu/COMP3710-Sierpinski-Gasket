65b5ec5f75feeb0140672b9da7b7b4487e0110dd
PeijieLyu <peijie.lyu@student.uq.edu.au>
2026-08-19 21:32:18 +0800

    complete code

--- sierpinski.py
+++ sierpinski.py
@@ -1,6 +1,9 @@
 import torch
+import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print("Using device:", device)
 
 size = 256
 
@@ -9,3 +12,20 @@ y = torch.arange(size, device=device)
 
 print("x shape:", x.shape)
 print("y shape:", y.shape)
+
+Y, X = torch.meshgrid(y, x, indexing="ij")
+
+print("X shape:", X.shape)
+print("Y shape:", Y.shape)
+
+difference = Y - X
+
+triangle_mask = X <= Y
+
+and_result = torch.bitwise_and(X, difference)
+
+sierpinski = triangle_mask & (and_result == 0)
+
+plt.imshow(sierpinski.cpu().numpy(), cmap="gray")
+plt.axis("off")
+plt.show()
